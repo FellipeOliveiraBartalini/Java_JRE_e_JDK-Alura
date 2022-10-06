9ea40c950f8f2198675681210c1a0bb50ac0d5c6
FellipeOliveiraBartalini <fellipe.oliveira.work@gmail.com>
2022-10-06 16:36:39 -0300

    Criando variavel numerica

---
+++ src/TestaVariaveis.java
@@ -0,0 +1,9 @@
+public class TestaVariaveis {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+
+        int idade = 37;
+
+        System.out.println(idade + " anos");
+    }
+}
