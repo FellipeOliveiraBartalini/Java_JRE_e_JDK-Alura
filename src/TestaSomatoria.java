b221cbc3e72300b456ef486c914ef0453420110e
FellipeOliveiraBartalini <fellipe.oliveira.work@gmail.com>
2022-10-07 11:18:11 -0300

    Testando while loop com progressão aritmética e Gauss

---
+++ src/TestaSomatoria.java
@@ -0,0 +1,14 @@
+public class TestaSomatoria {
+    public static void main(String[] args) {
+        int contador = 0;
+        int total = 0;
+
+        while (contador <= 10) {
+            System.out.println(total + " + " + contador + " = " + (total + contador));
+
+            total = total + contador;
+
+            contador++;
+        }
+    }
+}
